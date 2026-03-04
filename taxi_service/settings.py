a827ce47ab0737130c8c2e12ae8301e902b71662
Marek Drządzewski <dzadzumaster@gmail.com>
2026-03-04 18:36:06 +0100

    migrate bootstrap4 to crispy_bootstrap4

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
-    "bootstrap4",
+    "crispy_bootstrap4",
     "crispy_forms",
     "taxi",
 ]
